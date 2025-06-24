9e841014423e4e061f98d9929029fbbeb94481b4
Dipesh <99866329+pilladipesh33@users.noreply.github.com>
2025-06-24 18:17:49 +0530

    feat:sidebar default collapse

--- app/(chat)/layout.tsx
+++ app/(chat)/layout.tsx
@@ -13,7 +13,7 @@ export default async function Layout({
   children: React.ReactNode;
 }) {
   const [session, cookieStore] = await Promise.all([auth(), cookies()]);
-  const isCollapsed = cookieStore.get('sidebar:state')?.value !== 'true';
+  // const isCollapsed = cookieStore.get('sidebar:state')?.value !== 'true';
 
   return (
     <>
@@ -21,7 +21,7 @@ export default async function Layout({
         src="https://cdn.jsdelivr.net/pyodide/v0.23.4/full/pyodide.js"
         strategy="beforeInteractive"
       />
-      <SidebarProvider defaultOpen={!isCollapsed}>
+      <SidebarProvider defaultOpen={false}>
         <AppSidebar user={session?.user} />
         <SidebarInset>{children}</SidebarInset>
       </SidebarProvider>
