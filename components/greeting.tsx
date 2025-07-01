ef577c7fd33778ef01aec1533a2ab759e98337fc
Dipesh <99866329+pilladipesh33@users.noreply.github.com>
2025-07-01 17:44:12 +0530

    title changed

--- components/greeting.tsx
+++ components/greeting.tsx
@@ -1,26 +1,24 @@
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
 export const Greeting = () => {
-  return (
-    <div
-      key="overview"
-      className="max-w-3xl mx-auto md:mt-20 px-8 size-full flex flex-col justify-center items-center"
-    >
-      <motion.div
-        initial={{ opacity: 0, y: 10 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: 10 }}
-        transition={{ delay: 0.5 }}
-        className="text-2xl font-semibold"
-      >
-        Welcome to{' '}
-        <span className="text-purple-500 font-bold font-sans">
-          Portdex Chat
-        </span>
-        , I am your personal assistant.
-        <br /> &quot;I am here to help you with business ideas, market research,
-        crypto, finance, and more.&quot;
-      </motion.div>
-    </div>
-  );
+	return (
+		<div
+			key="overview"
+			className="max-w-3xl mx-auto md:mt-20 px-8 size-full flex flex-col justify-center items-center -mb-9"
+		>
+			<motion.div
+				initial={{ opacity: 0, y: 10 }}
+				animate={{ opacity: 1, y: 0 }}
+				exit={{ opacity: 0, y: 10 }}
+				transition={{ delay: 0.5 }}
+				className="text-4xl font-semibold"
+			>
+				Get started with{" "}
+				<span className="text-purple-500 font-bold font-sans">
+					Portdex Chat
+				</span>
+				<br /> A Financial and Web3.0 AI
+			</motion.div>
+		</div>
+	);
 };
