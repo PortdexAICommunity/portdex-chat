6e30e28fec44f1b44632aa216ec30bba4065679b
pilladipesh33 <pilladipesh.pd@gmail.com>
2025-08-05 16:31:14 +0530

    added the development branch deployed link

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -13,8 +13,16 @@ export const auth = defineAuth({
 				clientSecret: secret("GOOGLE_CLIENT_SECRET"),
 				scopes: ["email", "openid", "profile"],
 			},
-			callbackUrls: ["http://localhost:3000/", "https://chat.portdex.ai/"],
-			logoutUrls: ["http://localhost:3000/", "https://chat.portdex.ai/"],
+			callbackUrls: [
+				"http://localhost:3000/",
+				"https://chat.portdex.ai/",
+				"https://development.dgdo2awhfom3j.amplifyapp.com/",
+			],
+			logoutUrls: [
+				"http://localhost:3000/",
+				"https://chat.portdex.ai/",
+				"https://development.dgdo2awhfom3j.amplifyapp.com/",
+			],
 		},
 	},
 });
