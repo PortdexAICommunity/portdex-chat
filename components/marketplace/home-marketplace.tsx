49ae67d09e908cd2cbaa6a5c4b669fb7e83b3a04
pilladipesh33 <pilladipesh.pd@gmail.com>
2025-08-07 00:55:51 +0530

    typo fixed

--- components/marketplace/home-marketplace.tsx
+++ components/marketplace/home-marketplace.tsx
@@ -464,7 +464,7 @@ export const HomeMarketplace = () => {
             size="lg"
             className=""
           >
-            {showCount ? `Show More (${showCount} remaining)` : 'Counting...'}
+            {showCount ? `Show More (${showCount} remaining)` : 'Show More'}
           </Button>
         </motion.div>
 
