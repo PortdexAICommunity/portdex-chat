9dacbf5f90248b36c02e4082d520074370dec5f4
pilladipesh33 <pilladipesh.pd@gmail.com>
2026-02-04 20:16:35 +0530

    Revert "Merge branch 'production' into development"
    This reverts commit 8db74153fd13ea8afddb69b2efbb585faa17514b, reversing
    changes made to 39b12174b4348c2ed723ebae38ad0cdd022de90e.

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Entrypoint script to start the Next.js app, ensuring env vars are loaded at runtime
-
-# Print all environment variables for debugging (optional, remove if not needed)
-# printenv
-
-exec "$@"
